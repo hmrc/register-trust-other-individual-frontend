e48173f907b34cdd30d4cbcbc8f1df7f9d82cfdf
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-08-25 11:21:52 +0100

    TRUS-2271: updated route and registry key

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -30,7 +30,7 @@ class   FrontendAppConfig @Inject() (configuration: Configuration) {
   private val contactHost = configuration.get[String]("contact-frontend.host")
   private val contactFormServiceIdentifier = "trusts"
 
-  val repositoryKey: String = "naturalPerson"
+  val repositoryKey: String = "otherIndividuals"
 
   val analyticsToken: String = configuration.get[String](s"google-analytics.token")
   val analyticsHost: String = configuration.get[String](s"google-analytics.host")
