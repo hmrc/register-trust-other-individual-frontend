18a3b0fb71c9dec45b64a3ae8f3347dfbe81b1aa
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2021-05-27 12:36:21 +0100

    Update YesNoViewBehaviours, ViewSepcBase and InputYesNo component

--- test/views/ViewSpecBase.scala
+++ test/views/ViewSpecBase.scala
@@ -79,6 +79,9 @@ trait ViewSpecBase extends SpecBase {
     assert(doc.getElementById(id) == null, "\n\nElement " + id + " was rendered on the page.\n")
   }
 
+  def assertRenderedByClass(doc: Document, cssClass: String) =
+    assert(doc.getElementsByClass(cssClass) != null, "\n\nElement " + cssClass + " was not rendered on the page.\n")
+
   def assertNotRenderedByClass(doc: Document, className: String): Assertion = {
     assert(doc.getElementsByClass(className).isEmpty, "\n\nElement " + className + " was rendered on the page.\n")
   }
