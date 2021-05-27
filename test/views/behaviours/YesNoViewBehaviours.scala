eeeffa5a6c3b450932e82d015aa3fdb209ff0a6b
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2021-05-27 14:00:17 +0100

    Update YesNoViewBehaviours.yesNoPage

--- test/views/behaviours/YesNoViewBehaviours.scala
+++ test/views/behaviours/YesNoViewBehaviours.scala
@@ -41,7 +41,7 @@ trait YesNoViewBehaviours extends QuestionViewBehaviours[Boolean] {
 
           hintTextPrefix.map {
             pref =>
-              doc.getElementsByClass("form-hint").first.text must include(messages(s"$pref.hint"))
+              doc.getElementsByClass("govuk-hint").first.text must include(messages(s"$pref.hint"))
           }
         }
 
