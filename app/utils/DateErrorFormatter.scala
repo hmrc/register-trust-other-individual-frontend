3584c7e1852759bb38f6e04ca77dbcfddce6a177
Adam Conder <adamdconder@gmail.com>
2021-06-03 15:38:26 +0100

    Reformatting code

--- app/utils/DateErrorFormatter.scala
+++ app/utils/DateErrorFormatter.scala
@@ -26,11 +26,15 @@ object DateErrorFormatter {
     args.map(arg => if (dateArgs.contains(arg)) messages(s"date.$arg").toLowerCase else arg.toString)
   }
 
-  def addErrorClass(error: Option[FormError], dateArg: String)(implicit messages: Messages): String = {
+  def addErrorClass(error: Option[FormError], dateArg: String): String = {
     if(error.isDefined){
-    if(error.get.args.contains(dateArg) || error.get.args.isEmpty) s"govuk-input--error" else ""
-  } else {
-    ""
+      if(error.get.args.contains(dateArg) || error.get.args.isEmpty) {
+        s"govuk-input--error"
+      } else {
+        ""
+      }
+    } else {
+      ""
     }
   }
 
