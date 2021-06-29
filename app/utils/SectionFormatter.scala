8e3e1348b49727af73c04c5b17d0344aa94c07b9
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-06-29 20:40:57 +0100

    SPIKE - tidy up.

--- app/utils/SectionFormatter.scala
+++ app/utils/SectionFormatter.scala
@@ -16,10 +16,10 @@
 
 package utils
 
-import viewmodels.{AnswerRow, AnswerSection, RepeaterAnswerSection, Section}
+import play.api.i18n.Messages
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.{HtmlContent, Text}
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist._
-import play.api.i18n.Messages
+import viewmodels.{AnswerRow, AnswerSection, RepeaterAnswerSection, Section}
 
 object SectionFormatter {
 
@@ -39,8 +39,8 @@ object SectionFormatter {
           actions = Option(Actions(items = Seq(ActionItem(href=row.changeUrl.getOrElse(""),
             classes = s"change-link-$i",
             visuallyHiddenText = Some(messages(row.label, row.labelArg)),
-            content = Text(messages("site.edit")))))
-          )
+            content = Text(messages("site.edit"))
+          ))))
         )
     }
   }
