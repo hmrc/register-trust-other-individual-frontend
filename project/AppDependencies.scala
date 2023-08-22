229dd49f228165c096bc8a1b757a960595aa0008
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2023-08-22 12:41:08 +0100

    DDCE-4530: Changes as part of sm2 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,24 +2,24 @@ import sbt._
 
 object AppDependencies {
   import play.core.PlayVersion
-  val bootstrapVersion = "7.15.0"
+  val bootstrapVersion = "7.21.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "6.2.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
-    "uk.gov.hmrc"       %% "domain"                         % "8.1.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
+    "uk.gov.hmrc"       %% "domain"                         % "8.3.0-play-28",
     "com.typesafe.play" %% "play-json-joda"                 % "2.9.4",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"              %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.scalatest"            %% "scalatest"               % "3.2.15",
-    "org.scalatestplus"        %% "scalacheck-1-17"         % "3.2.15.0",
+    "org.scalatest"            %% "scalatest"               % "3.2.16",
+    "org.scalatestplus"        %% "scalacheck-1-17"         % "3.2.16.0",
     "org.scalatestplus.play"   %% "scalatestplus-play"      % "5.1.0",
-    "com.vladsch.flexmark"     %  "flexmark-all"            % "0.64.0",
-    "org.jsoup"                %  "jsoup"                   % "1.15.4",
+    "com.vladsch.flexmark"     %  "flexmark-all"            % "0.64.8",
+    "org.jsoup"                %  "jsoup"                   % "1.16.1",
     "com.typesafe.play"        %% "play-test"               % PlayVersion.current,
     "org.mockito"              %% "mockito-scala"           % "1.17.14",
     "org.scalacheck"           %% "scalacheck"              % "1.17.0",
