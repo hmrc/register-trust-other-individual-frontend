10b8270e0bd2602b77d36f25f38cf9909349265c
harish-korada1 <174133905+harish-korada1@users.noreply.github.com>
2025-01-30 10:59:06 +0000

    DDCE-6283: bot comment changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,8 +8,7 @@ object AppDependencies {
     "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "9.11.0",
     "uk.gov.hmrc"         %% "domain-play-30"                         % "9.0.0",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "2.0.0",
-    "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % playBootstrapVersion,
-    "uk.gov.hmrc"         %% "emailaddress-play-30"                   % "4.0.0"
+    "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % playBootstrapVersion
   )
 
   private lazy val test = Seq(
